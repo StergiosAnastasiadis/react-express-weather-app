bdcd59101d04c827f6c1b17721db490b728b81a3
StergiosAnastasiadis <Stelioslonx@outlook.com>
2022-01-15 23:28:14 +0200

    code-refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -4,44 +4,48 @@ import "./App.css";
 function App() {
 
   const [country, setCountry] = useState("");
-  const [degree, setDegree] = useState("");
-  const [name, setName] = useState("");
-  const [main, setMain] = useState("");
-  const [description, setDescription] = useState("");
-  const [url, setUrl] = useState("");
+  const [weatherValues, setWeatherValues] = useState(
+    {
+      degree: "",
+      name: "",
+      main: "",
+      description: "",
+      url: ""
+    }
+  )
 
   const getData = async () => {
     const rawData = await fetch(`https://weather-app-38.herokuapp.com/api/${country}`)
 
     if (rawData.status === 404) {
-      setName("Something went wrong");
-      setDegree(`Your input was: ${country}`);
-      setMain("");
-      setDescription("");
-      setUrl("");
+      setWeatherValues({
+        name: "Something went wrong",
+        degree: `Your input was: ${country}`,
+      })
       console.log("Error");
     } else {
 
       const data = await rawData.json();
-
-      setName(data.name);
       let degree = parseInt(data.main.temp);
-      setDegree(degree + " °C");
-      setMain(data.weather[0].main);
-      setDescription(data.weather[0].description);
       let iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}` + ".png";
-      setUrl(iconURL);
+      setWeatherValues({
+        name: data.name,
+        degree: degree,
+        main: data.weather[0].main,
+        description: data.weather[0].description,
+        url: iconURL
+      })
     }
   }
 
-  function inputValidation(event) {
+  const inputValidation = (event) => {
     let text = event.target.value;
     text = text.replace(/[0-9]/g, '');
     text = text.replace(/[^\w\s]/gi, '');
     setCountry(text);
   }
 
-  function handleClick(event) {
+  const handleClick = (event) => {
     getData();
     setCountry("");
     event.preventDefault();
@@ -53,16 +57,13 @@ function App() {
         <input onChange={inputValidation} type="text" placeholder="Search" value={country} autofocus="autofocus"></input>
         <button onClick={handleClick}>&#10148;</button>
       </form>
-      <h1>{name}</h1>
-      <h2>{degree}</h2>
-      <h2>{main}</h2>
-      <h3>{description}</h3>
-      <img src={url} alt=""></img>
+      <h1>{weatherValues.name}</h1>
+      <h2>{weatherValues.degree}</h2>
+      <h2>{weatherValues.main}</h2>
+      <h3>{weatherValues.description}</h3>
+      <img src={weatherValues.url} alt=""></img>
     </div>
   );
 }
 
 export default App;
-
-// Array Destructuring
-// Complex state
